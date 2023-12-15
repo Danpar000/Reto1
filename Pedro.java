4cbe9a8735c23c4c8633813e257728176951dc20
PedSem <153596408+PedSem@users.noreply.github.com>
2023-12-15 09:43:03 +0100

    Create Pedro.java

---
+++ Pedro.java
@@ -0,0 +1,23 @@
+public static String pedirmascara (int metodo) {
+        String mascara = "";
+        Scanner sc = new Scanner(System.in);
+        try {
+          for(int i=0;i>=0 && i<2;i++){
+            boolean distinguirmetodo=false;
+            System.out.println("Introduce otra vez la máscara(0/1)");
+            mascara=sc.next();
+            if (metodo == 0) {
+                System.out.print("Introduce la máscara en notación decimal: ");
+                mascara = sc.next();
+            } else if (metodo == 1) {
+                System.out.println("Introduce la máscara en notación CDIR");
+                mascara = sc.next();
+            }
+        } catch (Exception e) {
+            System.out.println("Ha ocurrido un error.");
+        }
+        return mascara;
+}
+public static void main(String[]args){
+  
+}
