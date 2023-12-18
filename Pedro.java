a048d40fb8fc2a423d461bcccddae66377dbb7b7
PedSem <153596408+PedSem@users.noreply.github.com>
2023-12-18 11:08:06 +0100

    Update Pedro.java

--- Pedro.java
+++ Pedro.java
@@ -1,11 +1,11 @@
 public static String pedirmascara (int metodo) {
         String mascara = "";
         Scanner sc = new Scanner(System.in);
-        try {
-          for(int i=0;i>=0 && i<2;i++){
+        for(int i=0;i>=0 && i<2;i++){
             boolean distinguirmetodo=false;
             System.out.println("Introduce otra vez la máscara(0/1)");
             mascara=sc.next();
+        try {
             if (metodo == 0) {
                 System.out.print("Introduce la máscara en notación decimal: ");
                 mascara = sc.next();
